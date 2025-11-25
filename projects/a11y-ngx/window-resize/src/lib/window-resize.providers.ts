8ea9af22b8d28030df2d6e987d99523003218dd1
Luciano Del Vacchio <delvacchio@hotmail.com>
2025-11-25 10:33:58 -0300

    Window Resize - Fixed token name

--- projects/a11y-ngx/window-resize/src/lib/window-resize.providers.ts
+++ projects/a11y-ngx/window-resize/src/lib/window-resize.providers.ts
@@ -3,7 +3,7 @@ import { InjectionToken } from '@angular/core';
 // Token to inject window object when available.
 // Primarily not to break SSR (Server Side Rendering) projects.
 export const WINDOW: InjectionToken<Window | undefined> = new InjectionToken<Window | undefined>(
-    'Color Scheme Window Token',
+    'Window Resize Window Token',
     {
         providedIn: 'root',
         factory: (): Window | undefined => (typeof window !== 'undefined' ? window : undefined),
